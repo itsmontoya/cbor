20184d4691f29620b556dd21e3b3ede98d4cf67c
Josh Montoya <josh@montoya.io>
2025-12-06 17:22:59 -0800

    implement typeCache

---
+++ typeCache.go
@@ -0,0 +1,30 @@
+package cbor
+
+import (
+	"reflect"
+	"sync"
+)
+
+var tc typeCache
+
+type typeCache struct {
+	mux sync.RWMutex
+	m   map[reflect.Type]encoderFn
+}
+
+func (c *typeCache) Get(t reflect.Type) (fn encoderFn, ok bool) {
+	c.mux.RLock()
+	defer c.mux.RUnlock()
+	fn, ok = c.m[t]
+	return
+}
+
+func (c *typeCache) Set(t reflect.Type, fn encoderFn) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	if _, ok := c.m[t]; ok {
+		return
+	}
+
+	c.m[t] = fn
+}
