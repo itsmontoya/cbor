0bdb5505a6018f2db12dd1cce9c4ac5153645ca8
Josh Montoya <josh@montoya.io>
2025-12-10 20:47:00 -0800

    add decoder for type cache

--- typeCache.go
+++ typeCache.go
@@ -5,21 +5,24 @@ import (
 	"sync"
 )
 
-var tc typeCache = typeCache{m: make(map[reflect.Type]encoderFn)}
+var (
+	etc typeCache[encoderFn] = typeCache[encoderFn]{m: make(map[reflect.Type]encoderFn)}
+	dtc typeCache[decoderFn] = typeCache[decoderFn]{m: make(map[reflect.Type]decoderFn)}
+)
 
-type typeCache struct {
+type typeCache[v any] struct {
 	mux sync.RWMutex
-	m   map[reflect.Type]encoderFn
+	m   map[reflect.Type]v
 }
 
-func (c *typeCache) Get(t reflect.Type) (fn encoderFn, ok bool) {
+func (c *typeCache[v]) Get(t reflect.Type) (fn v, ok bool) {
 	c.mux.RLock()
 	defer c.mux.RUnlock()
 	fn, ok = c.m[t]
 	return
 }
 
-func (c *typeCache) Set(t reflect.Type, fn encoderFn) {
+func (c *typeCache[v]) Set(t reflect.Type, fn v) {
 	c.mux.Lock()
 	defer c.mux.Unlock()
 	if _, ok := c.m[t]; ok {
